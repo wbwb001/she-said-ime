a72ced8284b00454a17445e25cf79582ebec9cb9
wbwb001 <wbwb001@users.noreply.github.com>
2026-07-25 00:24:09 +0800

    ui: 精简键盘布局 + 修复CMake版本

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 object Versions {
-    const val DEFAULT_CMAKE = "3.31.6"
+    const val DEFAULT_CMAKE = "4.1.2"
     const val DEFAULT_NDK = "28.0.13004108"
 
     val supportedAbis = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
